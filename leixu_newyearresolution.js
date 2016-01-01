33e039fd9312290b41763d78186c662909db741e
Lei Xu <leixu216@gmail.com>
2016-01-01 11:33:41 +0800

    Create leixu_newyearresolution.js

---
+++ leixu_newyearresolution.js
@@ -0,0 +1,9 @@
+// Author: Lei Xu (leixu@lean-soft.cn)
+// Date: Near Year Eve of 2016
+
+Year.replace ('2015', '2016').establishNewAndFreshStart(
+  new team(),
+  new idea(),
+  new effort()
+  ).toString('$');
+  
